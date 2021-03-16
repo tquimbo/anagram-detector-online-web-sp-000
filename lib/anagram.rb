590223ffaa21dc62c2655d532700753db53b34ff
Tim Quimbo <tquimbo1@gmail.com>
2021-03-16 00:44:44 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+# Your code goes here!
+require 'pry'
+class Anagram
+
+attr_accessor :word
+
+
+def initialize(word)
+  @word= word
+end
+
+
+def match(word)
+array = (%w(word))
+array.join.split("")
+array.sort
+array.each do |word|
+  if array == @word
+    return true
+  elsif array == []
+  end
+  end
+end
+binding.pry
+end
